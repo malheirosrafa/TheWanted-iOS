b3544c105309a699a0397bc9c505f463f1f4eeb9
Rafael Malheiros <rafael_malheiros@hotmail.com>
2015-12-04 02:05:01 -0200

    Initial Commit

---
+++ TheWanted-iOSTests/TheWanted_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  TheWanted_iOSTests.swift
+//  TheWanted-iOSTests
+//
+//  Created by rafael on 04/12/15.
+//  Copyright © 2015 playr2. All rights reserved.
+//
+
+import XCTest
+@testable import TheWanted_iOS
+
+class TheWanted_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
